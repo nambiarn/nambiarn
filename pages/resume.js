f4fb9ece802d470ea1fbffba4ecaf287bf5069e2
Nikhil Nambiar <nambiar.nikhil11@gmail.com>
2022-05-01 19:37:56 -0500

    Add canva border styling, fix use effect dependency issue

--- pages/resume.js
+++ pages/resume.js
@@ -8,7 +8,8 @@ import { commitDate } from '../generatedGitInfo.json';
 import Image from 'next/image';
 
 export default function Resume() {
-    const [name] = useState("Nikhil Nambiar");
+    const [firstName] = useState("Nikhil");
+    const [lastName] = useState("Nambiar")
     const [currentGigs, setCurrentGigs] = useState([]);
     const [lastUpdatedDate] = useState(commitDate)
     const [isSmallScreen, setIsSmallScreen] = useState(false);
@@ -25,7 +26,7 @@ export default function Resume() {
         return function cleanup() {
             mediaWatcher.removeEventListener('change', updateIsSmallScreen)
         }
-    })
+    }, [])
 
     useEffect(() => {
         setCurrentGigs(getCurrentGigs(experience))
@@ -33,33 +34,38 @@ export default function Resume() {
 
     return (
         <main className="outline">
-            <p className="flex flex-end italic">Last Updated: {lastUpdatedDate}</p>
-            <Image className="border-radius-50 margin-auto" src={profilePic} alt="Profile" width="120" height="120"/>
-            <h2 className="flex-center margin-bottom-0">{name}</h2>
-            <ProfileIcons/> 
-            <h3 className="font-weight-400 letter-spacing flex-center">{
-                currentGigs.map(currentGig => !isSmallScreen ?
-                    <span key={currentGig.id}> {currentGig.title} at {currentGig.company}
-                    {addPipe(currentGigs, currentGig)} </span> :
-                    <p className="flex-col" key={currentGig.id}> {currentGig.title} at {currentGig.company}</p>
+            <section className="section-area">
+                <p className="flex flex-end italic padding-top-sm">Last Updated: {lastUpdatedDate}</p>
+                <Image className="border-radius-50 margin-auto" src={profilePic} alt="Profile" width="120" height="120" />
+                <div aria-label="name">
+                    <div aria-label="first name" className="font-size-lg">{firstName}</div>
+                    <div aria-label="last name" className="font-size-lg name">{lastName}</div>
+                </div>
+                <ProfileIcons />
+                <h3 className="font-weight-400 letter-spacing flex-center">{
+                    currentGigs.map(currentGig => !isSmallScreen ?
+                        <span key={currentGig.id}> {currentGig.title} at {currentGig.company}
+                            {addPipe(currentGigs, currentGig)} </span> :
+                        <p className="flex-col" key={currentGig.id}> {currentGig.title} at {currentGig.company}</p>
                     )
                 }</h3>
-            <h2 className="border-bottom width-md">Experience</h2>
-            <section className="flex-space-around">
-                {
-                    experience.map((gig) => {
-                        return <Experience key={gig.id} title={gig.title} company={gig.company}
-                        startDate={gig.startDate} endDate={gig.endDate} isCurrent={gig.isCurrent} />
-                    })
-                }
-            </section>
-            <section className="">
-                <h2 className="border-bottom width-md">Education</h2>
-                {
-                    experience.map((el) => 
-                        <Experience key={el.id} title={el.title} company={el.company}
-                        isCurrent={el.isCurrent} />)
-                }
+                <h2 className="border-bottom width-md">Experience</h2>
+                <section className="flex-space-around">
+                    {
+                        experience.map((gig) => {
+                            return <Experience key={gig.id} title={gig.title} company={gig.company}
+                                startDate={gig.startDate} endDate={gig.endDate} isCurrent={gig.isCurrent} />
+                        })
+                    }
+                </section>
+                <section className="">
+                    <h2 className="border-bottom width-md">Education</h2>
+                    {
+                        experience.map((el) =>
+                            <Experience key={el.id} title={el.title} company={el.company}
+                                isCurrent={el.isCurrent} />)
+                    }
+                </section>
             </section>
         </main>
     )
